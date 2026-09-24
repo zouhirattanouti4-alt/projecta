87b2d89d021eb0d50e232a73380d81465b03d84f
Zouhir ATTANOUTI <zouhirattanouti4@gmail.com>
2026-09-24 02:48:30 +0100

    Adding the JWT Authentication Filter

--- src/main/java/com/zouhir/neobank/configuration/AppConfiguration.java
+++ src/main/java/com/zouhir/neobank/configuration/AppConfiguration.java
@@ -1,4 +1,22 @@
 package com.zouhir.neobank.configuration;
 
+
+import com.zouhir.neobank.user.UserRepository;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
 public class AppConfiguration {
+    private final UserRepository userRepository;
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow( () -> new UsernameNotFoundException("User not found"));
+    }
 }
--- src/main/java/com/zouhir/neobank/user/UserRepository.java
+++ src/main/java/com/zouhir/neobank/user/UserRepository.java
@@ -1,4 +1,12 @@
 package com.zouhir.neobank.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+     Optional<User> findByEmail(String userEmail);
+
 }
