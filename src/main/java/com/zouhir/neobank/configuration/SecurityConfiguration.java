800d9f2aeb8f9cfd229b4033ef582599a3048ea5
Zouhir ATTANOUTI <zouhirattanouti4@gmail.com>
2026-09-24 03:46:51 +0100

    feat : complet Security Filter Chain

--- src/main/java/com/zouhir/neobank/configuration/SecurityConfiguration.java
+++ src/main/java/com/zouhir/neobank/configuration/SecurityConfiguration.java
@@ -1,4 +1,42 @@
 package com.zouhir.neobank.configuration;
 
+import com.zouhir.neobank.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfiguration {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationProvider authenticationProvider) throws Exception{
+         http
+                .csrf(csrf -> csrf.disable())
+
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .sessionManagement( session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authenticationProvider(authenticationProvider)
+
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+        return http.build();
+
+
+    }
 }
